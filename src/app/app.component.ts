b15e63562178ff2136ecc84be1d83eabf7bd1285
SparshSingh10 <sparsh262002@gmail.com>
2025-03-24 11:58:27 +0530

    uc5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imageUrl = '../assets/BL_logo_square_png.png';
   url = 'https://www.bridgelabz.com/';
   userName: string = '';
+  nameError: string = '';
   ngOnInit() {
     this.title = 'Hello from BridgeLabz';
   }
@@ -17,6 +18,16 @@ export class AppComponent {
   onClick($event: MouseEvent) {
     console.log('Save button is clicked!', $event);
     window.open(this.url, '_blank');
+  }
+
+  onInput($event: Event) {
+    console.log('nput event fired! Changes have been made by User.', $event);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = '';
+      return;
+    }
+    this.nameError = 'Name is Incorrect!';
   }
 
 }
